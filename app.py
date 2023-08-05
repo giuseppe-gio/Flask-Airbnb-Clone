d1af11e9d0dd1a465b7e99e2d35ddf4e9e19e002
giuseppe-gio <141492864+giuseppe-gio@users.noreply.github.com>
2023-08-05 18:14:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ def get_listings():
     url= "https://www.airbnb.it/s/Italia/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2023-09-01&monthly_length=3&price_filter_input_type=0&price_filter_num_nights=5&channel=EXPLORE&query=Italia&place_id=ChIJA9KNRIL-1BIRb15jJFz1LOI&date_picker_type=calendar&source=structured_search_input_header&search_type=autocomplete_click"
     response= requests.get(url)
     soup= BeautifulSoup(response.content, "lxml")
-
+#   listings_prices = {}
     listings_data = []
     for item in soup.select('[itemprop=itemListElement]'):
             img= item.find("img", {"class":"itu7ddv i1mla2as i1cqnm0r dir dir-ltr"})
             foto = img['src']
+
+       #    price= item.find("span",{"class":" "}).getText()
             
             for name in item.select('[itemprop=name]'):
-                 listings_data.append((name['content'], foto)) 
+                 listings_data.append((name['content'], foto))
+               # listings_prices[name["content"]] = price
     print(listings_data)
 
 
